7715c2159a777d529484c70748935dd75c5ee1c1
me <1450638654@qq.com>
2019-11-14 14:05:38 +0800

    axios初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+// 配置axios全局使用
+Vue.prototype.$axios = axios
+// 配置基地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/'
+
 Vue.use(Element)
 Vue.config.productionTip = false
 
